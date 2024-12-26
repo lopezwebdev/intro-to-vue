ab53d23f53fd960f899d5f56a452894a8c3710b4
Esteban Lopez <esteban.steam21@gmail.com>
2024-12-26 13:53:22 +0900

    Update Tweet.js

--- Tweet.js
+++ Tweet.js
@@ -1,13 +1,5 @@
-const app = new Vue({
-  el: '#app',
-  data: {
-    username: 'CoderInTraining',
-    newTweet: '',
-    tweets: [
-      'Started learning to code today. Wish me luck!', 
-      'Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.', 
-      'Today I start learning Vue. I got this.'
-    ],
-    bio: 'Excited future front-end  engineer.'
-  }
+
+const Tweet = Vue.component('tweet', {
+ props: ['message', 'author'],
+ template: '<div class="tweet"><h3>{{ author }}</h3><p>{{ message }}</p></div>'
 });
